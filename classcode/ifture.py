47558f6fcab7fc573b0951e2a73e50a89f3f2175
tianya1993 <1461535061@qq.com>
2017-11-10 09:39:28 +0800

    modified:   classcode/.idea/workspace.xml 	new file:   classcode/ifture.py

---
+++ classcode/ifture.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+#-*-coding:utf8-*-
+"""请修改列表生成式，通过添加if语句保证列表生成式能正确地执行：
+L1 = ['Hello', 'World', 18, 'Apple', None]
+ print(L2)
+['hello', 'world', 'apple']
+ """
+
+""" 
+L = ['Hello', 'World', 18, 'Apple', None,"66"]
+#L=["list",6,8,"apple","docker","66"]
+
+L2=[]
+for index in L:
+    if  isinstance(index,str):
+        L2.append(index.lower())
+    else:
+        continue
+print(L2)
+"""
+L1 = ['Hello', 'World', 18, 'Apple', None,"66",[18, 'Apple', None,"66"]]
+L2 = [x.pop() for x in L1 if isinstance(x, (list))]
+print(L2)
