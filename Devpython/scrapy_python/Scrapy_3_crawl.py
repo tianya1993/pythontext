4622e14d2fd4be5322725a7bbeb2a462902e0bef
tianya1993 <1461535061@qq.com>
2017-11-30 21:02:30 +0800

    add file

--- Devpython/scrapy_python/Scrapy_3_crawl.py
+++ Devpython/scrapy_python/Scrapy_3_crawl.py
@@ -3,8 +3,10 @@
 import  urllib.request
 #file=urllib.request.urlretrieve("http://www.baidu.com","baidu.html")
 url="http://www.baidu.com"
-file=urllib.request.urlopen(url)
-data=file.read()
+#file=urllib.request.urlopen(url)
+req=urllib.request.Request(url)
+req.add_header("User-Agent","Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:57.0) Gecko/20100101 Firefox/57.0")
+data=urllib.request.urlopen(req).read()
 print(data)
 with open("baidu2.html","wb") as  filename:
 	filename.write(data)
